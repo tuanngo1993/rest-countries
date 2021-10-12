7864b2eaaec1116c6b1b0c62394e547975550f5a
Tuan Ngo <tuannaru1993@gmail.com>
2021-10-12 21:26:35 +0700

    use fetch instead of axios

--- src/modules/fetch-data.js
+++ src/modules/fetch-data.js
@@ -1,16 +1,13 @@
-import axios from 'axios'
-
 export async function fetchData(config = 'all') {
     try {
         const container = document.querySelector('.body')
         container.innerHTML =
             '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>'
 
-        const response = await axios.get(
-            `https://restcountries.com/v3/${config}`
-        )
+        const request = await fetch(`https://restcountries.com/v3/${config}`)
+        const response = await request.json()
 
-        return response.data
+        return response
     } catch (error) {
         console.log(error)
     }
